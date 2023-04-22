4d55bd899f3102ab7c7d03690ec9b3a9b1605d7c
pedroforadori <foradorif@gmail.com>
2023-04-22 12:40:59 -0300

    ajuste caminho imagens

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,6 +1,4 @@
 import Image from "next/image";
-import camisetaPalmeiras from '/public/assets/camiseta.jpg';
-import chaoVila from '/public/assets/chao-vila.jpg';
 
 export default function About() {
     return(
@@ -12,10 +10,10 @@ export default function About() {
                     acabaria resultando nessa história de amor que iremos contar abaixo:
                 </div>
                 <div>
-                    <Image src={camisetaPalmeiras} width={800} alt="foto casal" />
+                    <Image src='/../public/assets/camiseta.jpg' width={800} height={500} alt="foto casal" />
                 </div>
                 <div>
-                    <Image src={chaoVila} width={800} alt="foto casal" />
+                    <Image src='/../public/assets/chao-vila.jpg' width={800} height={500} alt="foto casal" />
                 </div>
                 <div className="flex justify-center items-center w-[800px] text-lg px-10">
                     Era uma bela quarta-feira de outubro em 2019, dia de Brasileirão, quando os dois decidiram ir assistir ao jogo do Palmeiras e Santos, 
@@ -35,10 +33,10 @@ export default function About() {
                      eles sempre souberam que viveriam uma história linda, no tempo deles, quando estivessem preparados para receber o amor um do outro!
                 </div>
                 <div>
-                    <Image src={chaoVila} width={800} alt="foto casal" />
+                    <Image src='/../public/assets/chao-vila.jpg' width={800} height={500} alt="foto casal" />
                 </div>
                 <div>
-                    <Image src={chaoVila} width={800} alt="foto casal" />
+                    <Image src='/../public/assets/chao-vila.jpg' width={800} height={500} alt="foto casal" />
                 </div>
                 <div className="flex justify-center items-center w-[800px] text-lg px-10">
                     Esse momento chegou, e eles não poderiam estar mais felizes de estar construindo essa história de amor. E isso é apenas o início 
