9fd39dd2770fec50323c7e69ea559ead8f016fa0
pedroforadori <foradorif@gmail.com>
2023-04-28 12:51:47 -0300

    ajuste mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import abelImg from '../../public/assets/aerea.jpeg'
 export default function Home() {
   return (
     <div>
-      <div className="h-screen flex justify-center items-center overflow-hidden max-sm:mt-32 max-sm:items-start">
+      <div className="flex justify-center items-center overflow-hidden max-sm:mt-32 max-sm:items-start">
         <div className="max-sm:p-2">
           <div className="text-white text-9xl flex flex-col justify-center items-center w-full max-sm:text-5xl max-sm:text-wedding-500">
             <motion.p 
