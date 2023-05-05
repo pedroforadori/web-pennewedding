33e879d9d75f4320bbbcbebeaa4fa5db54bb6844
unknown <foradorif@gmail.com>
2023-05-05 13:21:00 -0300

    finalizaçao versao 1 sobre a festa

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,8 +7,8 @@ export default function About() {
         <div className="bg-white flex items-center flex-col">
             <h1 className="font-aerotis text-wedding-500 text-7xl mt-12 max-sm:text-4xl">Sobre a Festa</h1>
             <div className="py-10 mx-2">
-                <div className="grid grid-cols-2 max-sm:grid-cols-1">
-                    <div className="flex flex-col mt-10 text-xl gap-2 justify-center items-center text-justify max-sm:text-xs">
+                <div className="grid grid-cols-2 max-sm:grid-cols-1 text-wedding-500">
+                    <div className="flex flex-col mt-10 text-xl gap-2 justify-center items-center text-justify max-sm:text-xs mx-2">
                         <p>
                             <span className="font-bold">Espaço: </span>Recanto Flor da Vila
                         </p>
@@ -19,12 +19,12 @@ export default function About() {
                         <p>
                             <span className="font-bold">Horário: </span> 16h30
                         </p>
-                        <p className="flex flex-row mt-4">
+                        <p className="flex flex-row gap-2 mt-4">
                             <Link href="https://waze.com/ul?a=share_drive&locale=pt_BR&sd=-v3ANDKhD9MZy0J23A-sd&env=row&utm_source=waze_app&utm_campaign=share_drive">
-                                <Image src="/assets/waze.png" alt="waze" width={40} height={40} />
+                                <Image src="/assets/waze.png" alt="waze" width={20} height={20} />
                             </Link>
                             <Link href="https://waze.com/ul?a=share_drive&locale=pt_BR&sd=-v3ANDKhD9MZy0J23A-sd&env=row&utm_source=waze_app&utm_campaign=share_drive">
-                                <Image src="/assets/google-maps.png" alt="waze" width={40} height={40} />
+                                <Image src="/assets/google-maps.png" alt="waze" width={20} height={20} />
                             </Link>
                         </p>
                     </div>
@@ -33,16 +33,29 @@ export default function About() {
                     </div>
                 </div>
                 
-                <div className="flex flex-col mt-10 text-xl justify-center items-center text-justify max-sm:text-xs">
+                <div className="flex flex-col mt-10 text-xl justify-center items-center text-justify max-sm:text-xs text-wedding-500">
                     <p>
                         <span className="font-bold">Traje: </span> Esporte Fino
                     </p>
                 </div>
-                <div className="flex flex-col mt-10 text-xl mb-28 justify-center items-center text-justify max-sm:text-xs">
+                <div className="flex flex-col mt-10 text-xl mb-60 justify-center items-center text-justify max-sm:text-xs text-wedding-500 font-bold">
                     Contato para locação de van
-                    <div className="flex flex-row">
+                    <div className="flex flex-wrap mt-2">
                         <AddressBook size={32} weight="thin" />
-                        
+                        <div className="text-xs font-semibold">
+                            <p>Gold Turismo</p>
+                            <p>11 97575-1709</p>
+                        </div>
+                        <AddressBook size={32} weight="thin" className="ml-3"/>
+                        <div className="text-xs font-semibold">
+                            <p>Breno</p>
+                            <p>11 97498-2426</p>
+                        </div>
+                        <AddressBook size={32} weight="thin" className="ml-3"/>
+                        <div className="text-xs font-semibold">
+                            <p>Andrisa Turismo</p>
+                            <p>11 97952-6266</p>
+                        </div>
                     </div>
                 </div>
                 
